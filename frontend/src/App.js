f5dd79c51270f5e45ca3b593a8ce0167e610ecc4
sonika_sharma <sonikalekhraj@gmail.com>
2023-03-07 19:56:06 +0530

    added changes

---
+++ frontend/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Buy from './components/Buy';
+import Register from './components/Register';
+import './App.css';
+import Nav from './components/Nav';
+import Details from './components/Details';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+
+function App() {
+  return (
+    <div>
+      
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/register" element={<Register />}></Route>
+          <Route path="/buy" element={<Buy />}></Route>
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path="/details/:id" element={<Details />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
